679b8cb78f2b5c68c257a430c07b17875f88e3c0
Justin Howard <justin@loko.ai>
2022-10-11 14:08:56 -0500

    timing reports when video completed.

--- midas_mediapipe.py
+++ midas_mediapipe.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from statistics import median
+from time import process_time
 from face import FaceDet
 from depth_midas import DepthEstimator
 from detectors import PersonDetector
@@ -26,6 +27,7 @@ class VidStream(object):
         self.writer = cv2.VideoWriter(str(output), self.fourcc, 20, (self.w, self.h // 2))
         self.status = False
         self.cnt = 0
+        self.performance = {'iris': list(), 'body': list(), 'depth': list()}
 
     def stream(self):
         while True:
@@ -36,20 +38,29 @@ class VidStream(object):
                 self.status, self.frame = self.video.read()
                 self.cnt += 1
                 print(f'Frame: {self.cnt}')
-                if self.cnt >= 230:
-                    self.video.release()
-                    self.writer.release()
-                    cv2.destroyAllWindows()
-                    break
+                # if self.cnt >= 230:
+                #     self.video.release()
+                #     self.writer.release()
+                #     cv2.destroyAllWindows()
+                #     break
                 if self.status == True:
                     if cv2.waitKey(1) & 0xff == ord('q'):
                         self.video.release()
                         self.writer.release()
                         break
                     self.face.mesh = None
+                    self.start = process_time()
                     self.detector.findIris(self.frame)
+                    self.end = process_time()
+                    self.performance['iris'].append(self.end - self.start)
+
+                    self.start = process_time()                    
                     depth_frame = self.estimator.predict(self.frame)
+                    self.end = process_time() 
+                    self.performance['depth'].append(self.end - self.start)
+                    # depth from iris
                     self.face.get_depth(depth_frame)
+                    self.face.rel2abs()                
                     if not self.face.mesh is None:
                         self.detector.visualize(self.frame)
                         # calculate distances
@@ -74,33 +85,41 @@ class VidStream(object):
                         ######################################
                         depth_frame = self.to_video_frame(depth_frame)
                         # write output to depth image
-                        self.face.rel2abs()
                         message = f'Relative Inverse Depth: {round(self.face.ri_depth, 2)}'
                         message2 = f'Absolute Depth: {round(self.face.abs_depth, 2)}'
-                        message3 = f'RMSE: {self.face.rmse()}'
-                        message4 = f'MAE: {self.face.mae()}'
+                        message3 = f'RMSE: {round(self.face.rmse(), 2)}'
+                        message4 = f'MAE: {round(self.face.mae(), 2)}'
                         messages = [message, message2, message3,  message4]
                         self.write_messages(messages, depth_frame)
                         self.write_output(depth_frame)
                     else:
+                        # update depth from head location
+                        self.face.rel2abs()
+                        # write output to rgb frame
                         message = 'Face not detected. Using body pose estimates.'
                         cv2.putText(self.frame, message, (70, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2, cv2.LINE_AA)
+                        self.start = process_time()
                         self.frame, head_pts = self.detector.findBody(self.frame)
+                        self.end = process_time() 
+                        self.performance['body'].append(self.end - self.start)
                         self.face.get_headw(head_pts[0], head_pts[1])
                         self.face.s2c_dist(median(self.face.head_measurements), self.face.head_w)
                         message2 = f'S2C dist (ft): {self.face.s2c_d}'
                         cv2.putText(self.frame, message2, (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
                         depth_frame = self.to_video_frame(depth_frame)
                         # write output to depth image
-                        self.face.rel2abs()
                         message = f'Relative Inverse Depth: {round(self.face.ri_depth, 2)}'
                         message2 = f'Absolute Depth: {round(self.face.abs_depth, 2)}'
-                        message3 = f'RMSE (abs vs s2c dist): {self.face.rmse()}'
-                        message4 = f'MAE: {self.face.mae()}'
+                        message3 = f'RMSE: {round(self.face.rmse(), 2)}'
+                        message4 = f'MAE: {round(self.face.mae(), 2)}'
                         messages = [message, message2, message3,  message4]
                         self.write_messages(messages, depth_frame)
                         self.write_output(depth_frame)
                 else:
+                    print('Performance stats in FPS:')
+                    print(f"Iris: {1//median(self.performance['iris'])}")
+                    print(f"Body: {1//median(self.performance['body'])}")
+                    print(f"Depth: {1//median(self.performance['depth'])}")
                     break
             else:
                 print(f'Sucessfully read {self.cnt} out of {self.video.get(7)} frames.')
