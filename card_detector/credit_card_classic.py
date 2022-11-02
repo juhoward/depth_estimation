39854df287dafda964b544c267cf0a16b966d030
justin <jhoward@digitaloptometrics.com>
2022-11-02 09:03:55 -0500

    adding validator and integrating it into credit card detector

--- card_detector/credit_card_classic.py
+++ card_detector/credit_card_classic.py
@@ -1,4 +1,20 @@
+'''
+Uses classical methods to detect the presence of a rectangle
+with the same aspect ratio as a credit card.
+
+Step 1: convert to grayscale
+Step 2: blur image to reduce noise
+Step 3: edge  detection (Canny used)
+Step 4: find contours
+Step 5: detector shapes close to rectangular
+Step 6: filter proposed rectangles by area
+Step 7: filter proposed rectangles by aspect ratio
+
+bounding boxes are returned and should be filtered once more to find those
+near the face.
+'''
 from shapedetector import ShapeDetector
+from validator import Validator
 import imutils
 import cv2
 import numpy as np
@@ -6,8 +22,11 @@ import numpy as np
 
 class CardDetector(object):
     def __init__(self) -> None:
+        # credit card dimensions (h, w)
         self.ratio = 53.98 / 85.6
+
     def get_threshold_vals(self, img, sigma = .33):
+        # helper that automatically finds threshold for Canny function
         med = np.median(img)
         lower = int(max(0, (1-sigma) * med))
         upper = int(min(255, (1 - sigma) * med))
@@ -32,6 +51,8 @@ class CardDetector(object):
             cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
         sd = ShapeDetector()
+        # holder for boxes that meet basic criteria
+        proposals = []
         # loop over the contours
         for c in cnts:
             M = cv2.moments(c)
@@ -43,23 +64,18 @@ class CardDetector(object):
 
                 shape = sd.detect(c)
                 if shape == "card":
-                    # multiply the contour (x, y)-coordinates by the resize ratio,
-                    # then draw the contours and the name of the shape on the image
-                    # c = c.astype("float")
-                    # c *= ratio
-                    # c = c.astype("int")
+                    # fit box to countour
                     box = cv2.minAreaRect(c)
-                    # width * height
-                    # area = box[1][0] * box[1][1]
                     # fit to height / width ratio of cards
                     aspect_ratio = box[1][1] / box[1][0]
                     if abs(aspect_ratio - self.ratio) < .05:
                         box = cv2.boxPoints(box)
                         box = np.int0(box)
+                        proposals.append(box)
                         cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
                         cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
                             0.5, (255, 255, 255), 2)
         # show the output image
         # cv2.imshow("Image", image)
         # cv2.imshow("Image", edges)
-        return box
+        return proposals
